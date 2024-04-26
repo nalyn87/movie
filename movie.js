c23c6fd4a99c85d03ad0224e30bab31e942837f4
nalyn87 <nalyn0807@gmail.com>
2024-04-26 18:06:30 +0900

    4.26 완성 겸 저녁 푸쉬

--- movie.js
+++ movie.js
@@ -8,13 +8,14 @@ const options = {
     }
 };
 
+const movieData = [];
+
 async function getdata() {
     const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options);
     const data = await response.json();
 
 
-    const movieData = [];
-
+    // api key 뽑기
     for (item of data['results']) {
         const movie = {};
         movie['title'] = item['title'];
@@ -29,16 +30,16 @@ async function getdata() {
 };
 
 // 카드 만들기
-function makeCard(item) {
+function makeCard(item, count) {
 
     const innerHTML = `
-    <div class="col">
+    <div class="col" id="movieCard${count}">
         <div class="card h-100">
-            <div onclick="alert('영화 id: ${item.id}')">
+            <div onclick="alert('영화 id: ${item.movie_id}')">
                 <img src="https://image.tmdb.org/t/p/w500${item.poster_path}" class="card-img-top" alt="이미지 준비중">
             </div>
             <div class="card-body">
-                <h5 class="card-title">제목: ${item.title}</h5>
+                <h5 class="card-title" id="movieTitle${count}">제목: ${item.title}</h5>
                 <p class="card-text">줄거리: ${item.overview}</p>
             </div>
             <div class="card-footer">
@@ -51,7 +52,34 @@ function makeCard(item) {
 }
 
 // 검색 구현
+function movieSearch() {
+    // 검색한 값
+    const ex = document.querySelector("#search").value.toLowerCase();
 
+    // 검색한 값과 영화 제목 비교
+    const searchedData = movieData.filter((i) => {
+        if(i['title'].toLowerCase().search(ex) !== -1) {
+            return i['title'];
+        }
+    });
+
+    let num = 0;
+
+    for (let count = 0; count<20; count++) {
+        const movieCardDiv = document.querySelector(`#movieCard${count}`);
+        const movieTitle = document.querySelector(`#movieTitle${count}`);
+
+        if (searchedData[num]['title'] === movieTitle.innerHTML.substr(4,1000)) {
+            console.log(searchedData[num]['title'], movieTitle.innerHTML.substr(4,1000))
+            movieCardDiv.setAttribute("style", "display: block;")
+            if(searchedData.length-1 > num) {num++};
+        } else {
+            movieCardDiv.setAttribute("style", "display: none;")
+        }
+
+    }
+
+}
 
 
 
@@ -62,9 +90,14 @@ const print = async () => {
     const data = await getdata();
     let count = 0;
     data.forEach(item => {
-        makeCard(item);
-        // console.log(item);
+        makeCard(item, count);
+        count++;
     });
-}
+    
+    document.getElementById("searchbtn").addEventListener("click", movieSearch);
+    document.getElementById("searchbtn").addEventListener("keydown", event => {
+        if (event.key == 'Enter') {movieSearch()};
+    });
+} 
 
 print();
