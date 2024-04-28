61fec3d6c5db6d64d81589cc56c554db864a1a30
nalyn87 <nalyn0807@gmail.com>
2024-04-28 22:54:49 +0900

    4.28 그동안 수정한거 푸쉬

--- movie.js
+++ movie.js
@@ -23,6 +23,7 @@ async function getdata() {
         movie['poster_path'] = item['poster_path'];
         movie['vote_average'] = item['vote_average'];
         movie['movie_id'] = item['id'];
+        movie['original_title'] = item['original_title'];
 
         movieData.push(movie);
     };
@@ -31,30 +32,36 @@ async function getdata() {
 
 // 카드 만들기
 function makeCard(item, count) {
+    // 오리지널 타이틀을 넣으려 했던 흔적 (실패함)
+    // function checkOriginalTitle() {
+    //     if (item.title !== item.original_title) {
+    //         return "(" + item.original_title + ")";
+    //     };
+    // }
 
-    const innerHTML = `
+    const movieDiv = `
     <div class="col" id="movieCard${count}">
         <div class="card h-100">
             <div onclick="alert('영화 id: ${item.movie_id}')">
                 <img src="https://image.tmdb.org/t/p/w500${item.poster_path}" class="card-img-top" alt="이미지 준비중">
             </div>
             <div class="card-body">
-                <h5 class="card-title" id="movieTitle${count}">제목: ${item.title}</h5>
-                <p class="card-text">줄거리: ${item.overview}</p>
+                <h5 class="card-title" id="movieTitle${count}">${item.title}</h5>
+                <p class="card-text">${item.overview}</p>
             </div>
             <div class="card-footer">
-                <small class="text-body-secondary">평점: ${item.vote_average}</small>
+                <small class="text-body-secondary">★ ${item.vote_average}</small>
             </div>
         </div>
     </div>
     `;
-    document.querySelector("#moviecard").insertAdjacentHTML('beforeend', innerHTML);
+    document.querySelector("#moviecard").insertAdjacentHTML('beforeend', movieDiv);
 }
 
 // 검색 구현
 function movieSearch() {
     // 검색한 값
-    const ex = document.querySelector("#search").value.toLowerCase();
+    const ex = document.querySelector("#searchbar").value.toLowerCase();
 
     // 검색한 값과 영화 제목 비교
     const searchedData = movieData.filter((i) => {
@@ -63,17 +70,18 @@ function movieSearch() {
         }
     });
 
+    // 영화 보이기/안보이기
     let num = 0;
 
     for (let count = 0; count<20; count++) {
         const movieCardDiv = document.querySelector(`#movieCard${count}`);
         const movieTitle = document.querySelector(`#movieTitle${count}`);
 
-        if (searchedData[num]['title'] === movieTitle.innerHTML.substr(4,1000)) {
-            console.log(searchedData[num]['title'], movieTitle.innerHTML.substr(4,1000))
+        if (searchedData[num]['title'] === movieTitle.innerHTML) {
             movieCardDiv.setAttribute("style", "display: block;")
             if(searchedData.length-1 > num) {num++};
         } else {
+            console.log(searchedData[num]['title'], movieTitle.innerHTML);
             movieCardDiv.setAttribute("style", "display: none;")
         }
 
@@ -95,7 +103,8 @@ const print = async () => {
     });
     
     document.getElementById("searchbtn").addEventListener("click", movieSearch);
-    document.getElementById("searchbtn").addEventListener("keydown", event => {
+    document.getElementById("search").focus();
+    document.getElementById("search").addEventListener('keydown', event => {
         if (event.key == 'Enter') {movieSearch()};
     });
 } 
